bc79d65c16be8ee787a88cd09f39b6cae404bd1e
PedroSodreUFES <pedro.malini@edu.ufes.br>
2025-11-02 19:42:37 -0300

    feat: sintaxe

---
+++ src/je05_tipos_wrppers/TiposWrappers.java
@@ -0,0 +1,20 @@
+package je05_tipos_wrppers;
+
+public class TiposWrappers {
+    public static void main(String[] args) {
+        // conversão implícita
+        double n = 123.99;
+        int nint = (int) n;
+        System.out.println(nint);
+
+        // conversão explícita
+        Double n2 = 100.99;
+        Integer nint2 = n2.intValue();
+        System.out.println(nint2);
+
+        // vantagem em escrita
+        String word = "1000";
+        Integer n3 = Integer.parseInt(word);
+        System.out.println(n3 + 1);
+    }
+}
